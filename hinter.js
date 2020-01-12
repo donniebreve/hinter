87c1e4af9c6606a23d6c7cc81058709583f08efb
donniebreve <donniebreve@gmail.com>
2020-01-11 23:04:37 -0800

    we good

--- hinter.js
+++ hinter.js
@@ -1,8 +1,10 @@
 let activationKey = 70; // f
 let selector = 'a,button,input,textarea,area,[contenteditable=true],[contenteditable=""],[tabindex],[role="button"],summary';
 let captureKeyboard = false;
+let focused = false;
 let links = [];
 let hints = {};
+let focus = undefined;
 let characters = [
     's', 'd', 'u', 'h', 'j', 'k', 'l', 'g', 'a', 'v', 'n', 't',
     'y', 'r', 'b', 'm', 'o', 'w', 'e', 'c', 'x', 'z', 'p', 'q'
@@ -64,17 +66,16 @@ function createHints() {
         let hintText = characters[Math.floor(i / 24)] + characters[(i % 24)];
         let hint = document.createElement('div');
         hint.className = 'hinter';
-        hint.style.left = rectangle.left + 'px';
-        hint.style.top = rectangle.top + 'px';
-        hint.style.width = rectangle.width + 'px';
-        hint.style.height = rectangle.height + 'px';
+        hint.style.top = (rectangle.top - 2) + 'px';
+        hint.style.left = (rectangle.left - 4) + 'px';
+        hint.style.height = (rectangle.height - 6) + 'px';
+        hint.style.width = (rectangle.width - 10) + 'px';
         hint.innerText = hintText;
         hints[hintText] = {
             element: hint,
             target: element
         };
         element.parentElement.insertBefore(hint, element);
-        //element.appendChild(hint);
     }
     keys = [];
     captureKeyboard = true;
@@ -84,21 +85,40 @@ function createHints() {
  * Clicks the target element.
  * @param {element} target
  */
-async function follow(target) {
+function follow(target) {
+    let rectangle = target.getBoundingClientRect();
+    let element = document.createElement('div');
+    element.className = 'hinter-focus';
+    element.style.top = rectangle.top + 'px';
+    element.style.left = rectangle.left + 'px';
+    element.style.width = (rectangle.width - 1) + 'px';
+    element.style.height = (rectangle.height - 1) + 'px';
+    target.parentElement.insertBefore(element, target);
+    focus = {
+        element: element,
+        target: target
+    };
     target.focus();
-    //await sleep(400);
-    //target.click();
+    focused = true;
 }
 
 /**
- * Clear hints.
+ * Clear elements.
  */
 function clear() {
+    // clear focus
+    if (focus) {
+        focus.element.parentNode.removeChild(focus.element);
+        focus = undefined;
+    }
+    // clear hints
     for (let property in hints) {
         let element = hints[property].element;
         element.parentNode.removeChild(element);
     }
     hints = {};
+    // clear state
+    focused = false;
     captureKeyboard = false;
 }
 
@@ -106,30 +126,36 @@ function clear() {
  * Process key presses.
  * @param {event} event
  */
-function onKeyUp(event) {
+function onKey(event) {
+    if (focused && event.keyCode != 13) {
+        clear();
+    }
     let element = event.target;
     if (element.contentEditable == true) return;
     if (element.tagName.toLowerCase() == "input") return;
     if (element.tagName.toLowerCase() == "textarea") return;
+    if (event.keyCode == 27) {
+        clear();
+        return;
+    }
     if (captureKeyboard) {
         event.stopPropagation();
         event.preventDefault();
-        console.log('stop');
-        if (event.keyCode == activationKey || event.keyCode == 27) {
-            clear();
-            return;
-        }
         keys.push(String.fromCharCode(event.keyCode).toLowerCase());
         if (keys.length > 2) keys = keys.slice(-2);
         let value = keys.join('');
         if (hints[value]) {
-            follow(hints[value].target);
+            let target = hints[value].target;
             clear();
+            follow(target);
             return;
         }
     }
-    if (event.keyCode == activationKey) {
+    if (event.ctrlKey
+        && event.shiftKey
+        && event.keyCode == activationKey) {
         createHints();
     }
 }
-document.body.addEventListener('keyup', onKeyUp, true);
+
+document.body.addEventListener('keydown', onKey, true);
